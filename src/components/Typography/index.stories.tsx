36ef47b7b700e322c70687ef579cbbd5b9e99f15
overthestream <njhnjh02@naver.com>
2021-07-29 19:38:28 +0900

    feat(Typography): add Storybook for typography

---
+++ src/components/Typography/index.stories.tsx
@@ -0,0 +1,38 @@
+import { Story } from '@storybook/react';
+import React from 'react';
+
+import Typography, { TypographyElementType, TypographyProps } from '.';
+
+export const Gallery: Story = () => {
+  const typographElements: TypographyElementType[] = [
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'h6',
+    'p',
+    'div',
+    'span',
+  ];
+  return (
+    <>
+      {typographElements.map((element) => (
+        <Typography elementType={element}>Lorem Ipsum</Typography>
+      ))}
+    </>
+  );
+};
+
+export const Template: Story<TypographyProps> = (args) => {
+  return <Typography {...args} />;
+};
+
+Template.args = {
+  children: 'Lorem Ipsum',
+};
+
+export default {
+  title: 'components/Typography',
+  component: Typography,
+};
