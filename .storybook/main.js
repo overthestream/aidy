c5fd2b2dabd85314c0266ecb03ce9f996fc85ce7
overthestream <njhnjh02@naver.com>
2021-07-29 19:38:07 +0900

    chore: initialize storybook

---
+++ .storybook/main.js
@@ -0,0 +1,19 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  stories: ['../src/components/**/*.stories.tsx'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async config => {
+    config.resolve.plugins.push(new TsconfigPathsPlugin());
+    return config;
+  },
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      shouldRemoveUndefinedFromOptional: true,
+      propFilter: prop =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
+  },
+};
